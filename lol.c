af0d68af3044ba9bd2a847cabcda6131d408246d
Gabriel Ferreira <gabriel_ferreira100@hotmail.com>
2021-12-03 20:40:10 -0300

    adcionado condicionais , mas ha um erro que ainda n consegui resolver

--- lol.c
+++ lol.c
@@ -9,9 +9,11 @@ int main()
   int vjogador2;
   int vjogador3;
 
-//2º modo de declarar variavel,na mesma linha
 
-  int djogador1; int djogador2; int djogador3;
+//2º modo de declarar variavel,na mesma linha
+//tipo variavelA,B,C
+
+  int djogador1,djogador2,djogador3;
   
   printf("quantas partidas o jogador 1 teve de vitorias ?\n");
   scanf("%d",&vjogador1);
@@ -37,12 +39,63 @@ int main()
     ....
     */
 
+   float mmr1;
+   float mmr2;
+   float mmr3;
+
+  mmr1= vjogador1*10 + djogador1*-10;
+  mmr2= vjogador2*10 + djogador2*-10;
+  mmr2= vjogador3*10 + djogador3*-10;
+
+
+
+//condional para jogadores jogarem juntos 
+
+//Perguntar o fabricio pq esta apresentando apenas as condiçoes 1ª e ultima (nao consegui entender onde pode estar errado)
+
+
+  if (mmr1==mmr2 && mmr2!= mmr3)
+  {
+    printf("apenas jogadores 1 e 2 podem jogar juntos\n");
+  }
+
+ if (mmr2==mmr3 && mmr2!=mmr1)
+ {
+    printf("apenas jogadores 2 e 3 podem jogar juntos\n");
+ }
+  
+   if (mmr1==mmr3 && mmr1!=mmr2)
+ {
+    printf("apenas jogadores 1 e 3 podem jogar juntos\n");
+ }
+
+ if (mmr1==mmr2==mmr3)
+ {
+   printf("partida compativel para todos jogadores\n");
+ }
+ 
+ if (mmr1!=mmr2!=mmr3)
+ {
+   printf("balanceamento nao compativel para o ranking dos jogadores\n");
+ }
+ 
+  
+
 //teste:
 printf("%d",vjogador1);
 printf("%d",djogador1);
 
 printf("%d",vjogador2);
 printf("%d",djogador2);
+
+printf("%d",vjogador3);
+printf("%d",djogador3);
+
+printf("%f",mmr1);
+printf("%f",mmr2);
+printf("%f",mmr3);
+
+
 
 
   
