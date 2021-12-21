3effeeb9c40404eaeed53c18e833368212ed2a09
Gabriel Ferreira <gabriel_ferreira100@hotmail.com>
2021-12-21 20:50:00 -0300

    adicionado vetor de mmr e condiçoes de jogadores jogarem junto analizando o mmr

--- lol.c
+++ lol.c
@@ -3,77 +3,103 @@
 
 int main()
 {
- //variaveis 
+//variaveis 
 
   int vitoriasjogador;
   int derrotasjogador;
   int quantidadejogadores;
   int contador = 1;
+  int i ; //usado para percorrer vetor e comparar mmr
+  int j ; //usado para percorrer vetor e comparar mmr
   float mmr;
+
 
   printf("quantos jogadores terao o mmr analisados?\n");
   scanf("%d",&quantidadejogadores);
 
+
+//vetores /arrays
+  int vetormmrs[quantidadejogadores];
+
+
 //repetidor - repetira o codigo para saber o mrr de jogador por jogador
+
+  printf("No total precisam ser analisadas 20 partidas de cada jogador\n");
 
 for (contador; contador < (quantidadejogadores + 1); contador++)
 {
+
   printf("quantas partidas o jogador %d teve de vitorias ?\n",contador);
   scanf("%d",&vitoriasjogador);
 
   printf("quantas partidas o jogador %d teve de derrotas ?\n",contador);
   scanf("%d",&derrotasjogador);
 
+
+
 /*  calculo mmr
     vitorias valem:10 pontos
     derrotas valem:-10 pontos */
 
+
   mmr = (vitoriasjogador * 10) + (derrotasjogador * -10);
+
+  vetormmrs[contador]=mmr;
   
   printf("o calculo de mmr seu apresentado eh de: %.1f\n",mmr);
  
   printf("\n************************\n");
 
+}
+
 
 //condional para jogadores jogarem juntos 
 
-//Perguntar o fabricio pq esta apresentando apenas as condiçoes 1ª e ultima (nao consegui entender onde pode estar errado)
+/*
+mmr igual podem jogar junto !!! OK 
 
-/* quardarei  as informaçoes nos vetores  para comparalas 
+mmr entre: (N-50) ou (N+50) !!! OK
 
-  if (mmr1==mmr2 && mmr2!= mmr3)
-  {
-    printf("apenas jogadores 1 e 2 podem jogar juntos\n");
+mmr acima de 50 de diferença ou abaixo de 50 de diferença - nao podem !!! OK
+*/
+
+for ( i = 1; i < (quantidadejogadores + 1) ; i++){
+  for ( j = 1; j < (quantidadejogadores + 1); j++) {
+    
+    if ((i!=j)){
+      
+    
+    
+    if (vetormmrs[i]==vetormmrs[j] )
+    {
+      printf("jogador %d pode jogar com jogador %d.\n",i,j);
+      break;
+    }
+    if ((vetormmrs[j]-50)<vetormmrs[i] && vetormmrs[i]<(vetormmrs[j]+50) )
+    {
+      printf("jogador %d pode jogar com jogador %d.\n",i,j);
+    }
+    if (vetormmrs[i]>(vetormmrs[j]+50) || vetormmrs[i]<(vetormmrs[j]-50) )
+    {
+      printf("jogador %d nao podem jogar com jogador %d devido a despariedade de habilidade.\n",i,j);
+    }
+   }
   }
-
- if (mmr2==mmr3 && mmr2!=mmr1)
- {
-    printf("apenas jogadores 2 e 3 podem jogar juntos\n");
- }
   
-   if (mmr1==mmr3 && mmr1!=mmr2)
- {
-    printf("apenas jogadores 1 e 3 podem jogar juntos\n");
- }
-
- if (mmr1==mmr2==mmr3)
- {
-   printf("partida compativel para todos jogadores\n");
- }
- 
- if (mmr1!=mmr2!=mmr3)
- {
-   printf("balanceamento nao compativel para o ranking dos jogadores\n");
- } */ 
-
-
-
-
-
-
 }
+
+printf("\n");
+
+/*testando se vetor de mmr ta funcionando 100% 
+
+for (contador=1; contador < (quantidadejogadores + 1); contador++)
+{
+  printf("%d,",vetormmrs[contador]);
+}
+*/
 
   return 0;
 }
+
 
 
