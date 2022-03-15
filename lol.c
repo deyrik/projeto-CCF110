112d58e10b09fd75f2ef48c811ad98d4f80ffb00
Gabriel de Pádua <89361768+deyrik@users.noreply.github.com>
2022-03-14 22:50:09 -0300

    criado uma strutura de dados (struct of data) para cada jogador e retirada varias agr inuteis

--- lol.c
+++ lol.c
@@ -1,77 +1,89 @@
   #include <stdio.h>
   #include <stdlib.h>
 
+
+  //abreviações usadas: 
+  // qtd = quantidade
+
+  typedef struct tipojogador {
+
+    int vitorias;
+    int derrotas;
+    float mmr; 
+
+  }Tipo_Jogador;
+
+
   int main(){
 
-  int vitoriasjogador, derrotasjogador, quantidadejogadores;
+  int qtd_jogadores;
   int contador = 0;
   int i,j;
   float mmr;
 
 
   printf("quantos jogadores terao o mmr analisados?\n");
-  scanf("%d",&quantidadejogadores);
+  scanf("%d",&qtd_jogadores);
   printf("No total precisam ser analisadas 20 partidas de cada jogador\n");
 
-  int vetormmrs[quantidadejogadores];//guarda os mmrs 
+  int vetormmrs[qtd_jogadores];//guarda os mmrs 
+  Tipo_Jogador Jogador[qtd_jogadores];
 
 
-  for (contador; contador < quantidadejogadores ; contador++){
+  for(contador; contador < qtd_jogadores ; contador++){
 
-    printf("quantas partidas o jogador %d teve de vitorias ?\n",contador+1);
-    scanf("%d",&vitoriasjogador);
+    printf("De 20 partidas, quantas o jogador %d teve de vitorias ?\n",contador+1);
+    scanf("%d",&Jogador[contador].vitorias);
+    
+    Jogador[contador].derrotas = 20 - Jogador[contador].vitorias;
 
-    printf("quantas partidas o jogador %d teve de derrotas ?\n",contador+1);
-    scanf("%d",&derrotasjogador);
+    printf("portanto o jogador %d teve %d  de derrotas !\n",contador+1,Jogador[contador].derrotas);
+   
 
     /*  calculo mmr
     vitorias valem:10 pontos
     derrotas valem:-10 pontos */
-    mmr = (vitoriasjogador * 10) + (derrotasjogador * -10);
-    vetormmrs[contador]=mmr;
+
+    mmr = (Jogador[contador].vitorias * 10) + (Jogador[contador].derrotas * (-10)); //posso apagar essa linha e usar o caluclo direto na debaixo. (-1 variavel)
+    Jogador[contador].mmr = mmr;
   
     printf("o calculo de seu mmr apresentado eh de: %.1f\n",mmr);
     printf("\n************************\n");
   }
 
 
-  float matriz_jogadorx_jogador[quantidadejogadores][quantidadejogadores];
+  float matriz_jogadorx_jogador[qtd_jogadores][qtd_jogadores];
 
-  for ( i = 0; i < quantidadejogadores ; i++){ 
-    for ( j = 0; j < quantidadejogadores ; j++) {
+  for ( i = 0; i < qtd_jogadores ; i++){ 
+    for ( j = 0; j < qtd_jogadores ; j++){
     
       if ((i!=j)){
     
-        if (vetormmrs[i]==vetormmrs[j] )
-        {
+        if (Jogador[i].mmr==Jogador[j].mmr ){// como estava -> vetormmrs[i]==vetormmrs[j]
           matriz_jogadorx_jogador[i][j]=1;
         }
-        if ((vetormmrs[j]-50)<vetormmrs[i] && vetormmrs[i]<(vetormmrs[j]+50) ) //49 pontos pra baixo ou pra cima podem 
-        {
+        if ((Jogador[j].mmr-50)<Jogador[i].mmr && Jogador[i].mmr<(Jogador[j].mmr+50) ){//49 pontos pra baixo ou pra cima podem ...... como estava ->(vetormmrs[j]-50)<vetormmrs[i] && vetormmrs[i]<(vetormmrs[j]+50)
           matriz_jogadorx_jogador[i][j]=1;
         }
-        if (vetormmrs[i]>(vetormmrs[j]+50) || vetormmrs[i]<(vetormmrs[j]-50) ) //50 a mais ou a menos nao podem 
-        {
+        if (Jogador[i].mmr > (Jogador[j].mmr+50) || Jogador[i].mmr < (Jogador[j].mmr-50) ){ //50 a mais ou a menos nao podem     como estava -> vetormmrs[i]>(vetormmrs[j]+50) || vetormmrs[i]<(vetormmrs[j]-50)
           matriz_jogadorx_jogador[i][j]=0;
         }
       }
 
-      if (i==j)
-       {
+      if (i==j){
          matriz_jogadorx_jogador[i][j]=1;
-       }
+      }
     }
   
 }
 
   printf("podem jogar juntos os players:\n");
 
-  for ( i = 0; i < quantidadejogadores; i++){
-    for ( j = 0; j < quantidadejogadores; j++){
+  for ( i = 0; i < qtd_jogadores; i++){
+    for ( j = 0; j < qtd_jogadores; j++){
 
-      if (i!=j && i<j && matriz_jogadorx_jogador[i][j]==1)
-      {
-        printf("player %d e %d\n",i,j);
+      if (i!=j && i<j && matriz_jogadorx_jogador[i][j]==1){
+        printf("player %d e %d\n",(i+1),(j+1));
       }
     
     }
